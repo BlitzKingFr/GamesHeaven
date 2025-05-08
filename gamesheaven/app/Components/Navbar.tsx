14bf5e8a5f41b266c61727295384639fdf60a16a
Jevis Kafle <jeviskafle@gmail.com>
2025-05-08 19:26:18 +0545

    navbar clean

--- gamesheaven/app/Components/Navbar.tsx
+++ gamesheaven/app/Components/Navbar.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 const Navbar = () => {
   return (
-    <nav>
-        <div>
-            <ul>
-                <li><Link href="/NavElements/Home">Home</Link> </li>
-                <li><Link href="/NavElements/Contact">Contact</Link> </li>
-                <li><Link href="/NavElements/About">About us</Link> </li>
-                <li><Link href="/NavElements/FAQ">FAQ</Link> </li>
+    <nav className='w-full p-3 flex justify-between bg-[rgb(39,68,93)] items-center'>
+        <div className='w-full flex justify-end'>
+            <ul className='flex flex-row space-x-8 text-white'>
+                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/Home">Home</Link></li>
+                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/Contact">Contact</Link> </li>
+                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/About">About us</Link> </li>
+                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/FAQ">FAQ</Link> </li>
                 <button><Link href="#">Login</Link></button>
             </ul>
         </div>
