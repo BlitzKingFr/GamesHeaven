37327d6c0eb4563ff677286d26190ecfd1d32e58
Jevis Kafle <jeviskafle@gmail.com>
2025-05-08 19:10:38 +0545

    qw

--- gamesheaven/app/Components/Navbar.tsx
+++ gamesheaven/app/Components/Navbar.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
         <div>
             <ul>
-                <li>Home</li>
-                <li>Contact</li>
-                <li>About us</li>
-                <li>FAQ</li>
-                <button>Login</button>
+                <li><Link href="">Home</Link> </li>
+                <li><Link href="">Contact</Link> </li>
+                <li><Link href="">About us</Link> </li>
+                <li><Link href="">FAQ</Link> </li>
+                <button><Link href="#">Login</Link></button>
             </ul>
         </div>
     </nav>
