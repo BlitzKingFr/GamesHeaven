e2a7d1204f5266d5cfd229ae7640412db9d6dd09
Jevis Kafle <jeviskafle@gmail.com>
2025-06-14 20:27:21 +0545

    purple

--- gamesheaven/app/Components/Navbar.tsx
+++ gamesheaven/app/Components/Navbar.tsx
@@ -3,17 +3,22 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 const Navbar = () => {
   return (
-    <nav className='w-full p-3 flex justify-between bg-[rgb(39,68,93)] items-center'>
-        <div className='w-full flex justify-end'>
-            <ul className='flex flex-row space-x-8 text-white items-center'>
-                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/Home">Home</Link></li>
-                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/Contact">Contact</Link> </li>
-                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/About">About us</Link> </li>
-                <li className='hover:bg-yellow-400 hover:text-black p-3'><Link href="/NavElements/FAQ">FAQ</Link> </li>
-                <Button><Link href="#">Login</Link></Button>
-            </ul>
-        </div>
-    </nav>
+    <nav className='w-full p-3 flex justify-between bg-purple-700 items-center'>
+  <div className='w-full flex justify-end'>
+    <ul className='flex flex-row space-x-8 text-white items-center'>
+      <li className='hover:text-fuchsia-200 transition'><Link href="/NavElements/Home">Home</Link></li>
+      <li className='hover:text-fuchsia-200 transition'><Link href="/NavElements/Contact">Contact</Link></li>
+      <li className='hover:text-fuchsia-200 transition'><Link href="/NavElements/About">About us</Link></li>
+      <li className='hover:text-fuchsia-200 transition'><Link href="/NavElements/FAQ">FAQ</Link></li>
+      <li>
+        <Button className="text-white bg-fuchsia-600 hover:bg-fuchsia-700 transition">
+          <Link href="#">Login</Link>
+        </Button>
+      </li>
+    </ul>
+  </div>
+</nav>
+
   )
 }
 
